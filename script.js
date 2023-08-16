21a3ac3eb76341154772ee1e8b70ffc4e468100e
Tanmayjain0906 <tanmayjain983@gmail.com>
2023-08-16 17:23:40 +0530

    completed

---
+++ script.js
@@ -0,0 +1,153 @@
+let view = "grid";
+let gridButton = document.getElementById("grid");
+let listButton = document.getElementById("list");
+
+gridButton.classList.add("active");
+fetchData();
+
+gridButton.addEventListener("click", () => {
+  view = "grid";
+  gridButton.classList.add("active");
+  listButton.classList.remove("active");
+  fetchData();
+})
+
+listButton.addEventListener("click", () => {
+  view = "list";
+  listButton.classList.add("active");
+  gridButton.classList.remove("active");
+  fetchData();
+})
+
+let container = document.getElementsByClassName("container")[0];
+let str = "$";
+async function fetchData() {
+  let url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
+
+  let prom = await fetch(url);
+  let data = await prom.json();
+
+  loadData(data);
+}
+
+function loadData(data) {
+  container.innerHTML = ``;
+  if (view === "grid") {
+    for (let i = 0; i < data.length; i++) {
+      let card = document.createElement("div");
+      card.className = "card";
+
+      card.innerHTML = `<div id="head">
+  
+       <div class="currency-img">
+         <img src=${data[i].image} alt="" width="40px">
+       </div>
+       
+       <div id="name">
+         ${data[i].symbol.toUpperCase()}
+         <div id="caption">
+             ${data[i].name}
+          </div>
+       </div>
+     
+     </div>
+     
+     <div id="percent">
+       
+         <div class="per-btn">${Math.ceil(data[i].price_change_percentage_24h)}</div>
+     
+     </div>
+     
+     <div id="price">
+     <i class="fa-solid fa-dollar-sign" style="color: #75E338;"></i>
+     ${data[i].current_price}
+        
+         
+     </div>
+     
+     <div id="volume">
+       
+         Total Volume: ${data[i].total_volume}
+     
+     </div>
+     
+     <div id="cap">
+         
+         Market Cap: <i class="fa-solid fa-dollar-sign" style="color: #7F7F71;"></i>${data[i].market_cap}
+     
+     </div>`
+
+      container.appendChild(card);
+
+    }
+  }
+  else {
+    let table = document.createElement("table");
+    let thead = document.createElement("thead");
+    thead.className = "table-head";
+   
+
+    table.appendChild(thead);
+
+    let tbody = document.createElement("tbody");
+
+    for (let i = 0; i < data.length; i++) {
+      let tr = document.createElement("tr");
+      let td1 = document.createElement("td");
+      let td2 = document.createElement("td");
+      let td3 = document.createElement("td");
+      let td4 = document.createElement("td");
+      let td5 = document.createElement("td");
+
+      td1.innerHTML = `<div id="head">
+  
+      <div class="currency-img">
+        <img src=${data[i].image} alt="" width="40px">
+      </div>
+      
+      <div id="name">
+        ${data[i].symbol.toUpperCase()}
+        <div id="caption">
+            ${data[i].name}
+         </div>
+      </div>
+    
+    </div>`
+
+    td1.colSpan = "2";
+
+      td2.innerHTML = `<div id="percent">
+       
+    <div class="per-btn">${Math.ceil(data[i].price_change_percentage_24h)}</div>
+
+</div>`
+      td1.className = "td-1";
+
+      td3.innerHTML = ` <div id="price">
+      <i class="fa-solid fa-dollar-sign" style="color: #75E338;"></i>
+      ${data[i].current_price}
+         
+          
+      </div>`
+
+      td4.innerHTML = ` <div id="volume">
+       
+      ${data[i].total_volume}
+  
+  </div>`
+
+      td5.innerHTML = ` <div id="cap">
+         
+       <i class="fa-solid fa-dollar-sign" style="color: #7F7F71;"></i>${data[i].market_cap}
+  
+  </div>`
+
+      tr.append(td1, td2, td3, td4, td5);
+      tbody.appendChild(tr);
+    }
+
+    table.appendChild(tbody);
+    document.body.appendChild(table);
+  }
+}
+
